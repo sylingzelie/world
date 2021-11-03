a64770007b382be9d4ee1af50d92f089f20223e2
ze <1450287258@qq.com>
2021-11-03 14:57:23 +0800

    feat:添加swagger配置

---
+++ src/main/java/com/sy/world/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.sy.world.controller;
+
+import com.sy.world.entity.User;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(value = "用户管理",tags = "用户管理")
+@RestController
+public class UserController {
+
+
+    @ApiOperation("获取用户")
+    @GetMapping("getUser")
+    public User getUser(){
+        User user = new User();
+        user.setUsername("sy");
+        return user;
+    }
+}
---
+++ src/main/java/com/sy/world/entity/User.java
@@ -0,0 +1,12 @@
+package com.sy.world.entity;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+@Data
+@ApiModel(value = "用户信息")
+public class User{
+    @ApiModelProperty(value = "用户信息")
+    private String username;
+}
