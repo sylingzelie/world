cbafdd953b876c293a25d5e7406a13f9c329415c
ze <1450287258@qq.com>
2022-04-08 17:27:45 +0800

    test2

--- src/main/java/com/sy/world/controller/test.java
+++ src/main/java/com/sy/world/controller/test.java
@@ -4,4 +4,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("test");
     }
+    public static void maina() {
+        System.out.println("dev");
+    }
 }
